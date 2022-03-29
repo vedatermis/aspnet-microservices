c6c0e6ea821e3cf267989c50925a7e611494e937
Vedat Ermiş <vedat.ermis@kariyer.net>
2022-03-29 11:50:44 +0300

    Ordering.API

---
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Queries.GetOrdersList;
+using Ordering.Domain.Entities;
+
+namespace Ordering.Application.Mappings
+{
+    public class MappingProfile: Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+        }
+    }
+}
